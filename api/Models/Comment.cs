1ef8aa32476c2b695aabd695bc275aa4265c8731
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-26 13:33:02 +0100

    Done lesson 2

---
+++ api/Models/Comment.cs
@@ -0,0 +1,13 @@
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        // Navigation proprty
+        public Stock? Stock { get; set; }
+    }
+}
